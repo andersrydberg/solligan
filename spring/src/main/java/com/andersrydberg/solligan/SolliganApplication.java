da126827bf069db12e28d52a2e08fd3969d10e20
Anders Rydberg <andersrydberg@yahoo.com>
2023-06-18 20:47:50 +0200

    Initial spring configuration

---
+++ spring/src/main/java/com/andersrydberg/solligan/SolliganApplication.java
@@ -0,0 +1,13 @@
+package com.andersrydberg.solligan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolliganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolliganApplication.class, args);
+	}
+
+}
